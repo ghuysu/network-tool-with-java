c3408c16cb29eb042e823ece67e8d0ee19c96112
grilledfishu <ndtnhan2003@gmail.com>
2023-11-03 00:43:50 +0700

    Traceroute has been completed!

--- src/NetworkToolApp.java
+++ src/NetworkToolApp.java
@@ -315,36 +315,28 @@ public class NetworkToolApp {
 
     private static List<String> performTraceroute(String host) {
         List<String> results = new ArrayList<>();
-        try {
-            IcmpPingRequest request = IcmpPingUtil.createIcmpPingRequest();
-            request.setHost(host);
+        IcmpPingRequest request = IcmpPingUtil.createIcmpPingRequest();
+        request.setHost(host);
 
-            for (int ttl = 1; ttl <= 30; ttl++) {
-                request.setTtl(ttl);
-                IcmpPingResponse response = IcmpPingUtil.executePingRequest(request);
+        for (int ttl = 1; ttl <= 30; ttl++) {
+            request.setTtl(ttl);
+            IcmpPingResponse response = IcmpPingUtil.executePingRequest(request);
 
-                if (response.getTimeoutFlag()) {
-                    results.add(" *");
-                } else {
-                    String resultLine = ttl + ": " + response.getHost();
-                    if (response.getSuccessFlag()) {
-                        resultLine += " (RTT: " + response.getRtt() + " ms)";
-                    }
-                    results.add(resultLine);
-                }
+            if (response.getTimeoutFlag()) {
+                results.add(String.format("%2d     *        *        *        *", ttl));
+            } else {
+                String resultLine = String.format("%2d     %3d ms     %3d ms     %3d ms     %s", ttl, response.getRtt(), response.getRtt(), response.getRtt(), response.getHost());
+                results.add(resultLine);
 
-                if (response.getSuccessFlag() && response.getHost().equals(host)) {
+                if (response.getSuccessFlag()) {
                     results.add("Traceroute completed.");
                     break;
                 }
             }
-        } catch (Exception ex) {
-            results.add("Error: " + ex.getMessage());
         }
-        return results;
+
+        return results; // Trả về kết quả là danh sách các dòng traceroute
     }
-
-
 
     public static boolean isValidDomain(String domain) {
         // Sử dụng biểu thức chính quy để kiểm tra tên miền
